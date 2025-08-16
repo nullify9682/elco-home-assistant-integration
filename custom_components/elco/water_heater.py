135bb9be6acd4a3ed391e80a55d7abf57facf190
wizbot <platoon.06-banal@icloud.com>
2025-08-16 15:48:00 +0200

    Improve

--- custom_components/elco/water_heater.py
+++ custom_components/elco/water_heater.py
@@ -101,7 +101,7 @@ class ElcoWaterHeater(WaterHeaterEntity):
     async def async_set_operation_mode(self, operation_mode):
         if operation_mode not in self.operation_list:
             raise ValueError(f"Invalid operation mode {operation_mode}")
-        await self.hass.async_add_executor_job(self._api.set_dhw_operation_mode, 1 if operation_mode == STATE_ON else 0)
+        await self.hass.async_add_executor_job(self._api.set_dhw_operation_mode, 0 if operation_mode == STATE_OFF else 1)
         self._current_operation = operation_mode
         self.async_write_ha_state()
 
